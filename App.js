5c46001752cff9ddf3545cc34e2465afc4b65eb7
Christopher Marim <christopher.marim@etm.srv.br>
2020-03-07 20:37:44 -0300

    Stack and Labs

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import {  SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { StyleSheet, Button, Text, View } from 'react-native';
 
 import Routes from './src/routes';
 
 export default function App() {
   return (
+    <SafeAreaProvider>
     <NavigationContainer>
       <Routes />
     </NavigationContainer>  
+    </SafeAreaProvider>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
 import * as React from 'react';
 
-import { createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator, HeaderBackground} from '@react-navigation/stack';
 import {TransitionSpecs} from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {  SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
 
 import Scanner from './pages/Scanner';
 import Home from './pages/Home';
 import Collect from './pages/Collect'
 
 // const Stack = createStackNavigator();
-const Tab = createMaterialTopTabNavigator();
+  const Tab = createMaterialTopTabNavigator();
+    
+ 
+
+  
 
 const forFade = ({ current, closing }) => ({
     cardStyle: {
@@ -18,27 +24,48 @@ const forFade = ({ current, closing }) => ({
   });
   
   const HomeStack = createStackNavigator();
+    
+  
+    
+  
   
   function HomeStackScreen() {
     return (
       <HomeStack.Navigator>
         <HomeStack.Screen name="Home" component={Home} 
+        
+        
             options={
-                {title:'Página Inicial',
+                {headerShown: false,
+                  title:'Página Inicial',
                 transitionSpec: {
                     open: TransitionSpecs.TransitionIOSSpec,
                     close: TransitionSpecs.TransitionIOSSpec, 
                 },
-                cardStyleInterpolator: forFade
-        }}/>
+                cardStyleInterpolator: forFade,
+                
+                headerStyle:{
+                  backgroundColor:'#337DFF'
+                },
+                headerTintColor:'white',
+                screen:HomeStackScreen,
+                
+        }
+        
+        }/>
         <HomeStack.Screen name="Scanner" component={Scanner} 
-            options={{
+            options={{headerShown: false,
                 title:'Scanner',
                 transitionSpec: {
                     open: TransitionSpecs.TransitionIOSSpec,
                     close: TransitionSpecs.TransitionIOSSpec, 
                 },
-                cardStyleInterpolator: forFade
+                cardStyleInterpolator: forFade,
+                headerStyle:{
+                  backgroundColor:'#337DFF'
+                  
+                },
+                headerTintColor:'white',
         }} />
         
       </HomeStack.Navigator>
@@ -48,17 +75,44 @@ const forFade = ({ current, closing }) => ({
   function CollectStackScreen() {
     return (
         <Collect/>
-    );
+     );
   }
  
-  
+
+
+
+
+
 
 function Routes() {
     return(
-        <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeStackScreen} />
+      <SafeAreaView style={{flex:1, justifyContent:'space-between', alignItens:'center'}}>
+        
+        <Tab.Navigator tabBarPosition='top'
+        swipeEnabled='true'
+        animationEnable='enable'
+        tabBarOptions={{
+          inactiveTintColor: 'white',
+          activeTintColor:'white',
+          style:{backgroundColor:'#337DFF'},
+
+         indicatorStyle:{
+           borderBottomColor:"grey",
+           borderBottomWidth: 2
+         }, 
+          
+       labelStyle:{
+         fontSize:12,
+       },
+       
+      }}
+      
+        >
+        <Tab.Screen name="Home" component={HomeStackScreen}  />
         <Tab.Screen name="Collect" component={CollectStackScreen} />
       </Tab.Navigator>
+    
+      </SafeAreaView>
 
     );
 }
