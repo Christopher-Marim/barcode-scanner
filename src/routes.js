68b4500265f03b39d4edb872da8831a1750f128b
Victor Hugo <pavictorhugo@gmail.com>
2020-03-07 10:49:54 -0300

    criada estilização da navegação

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,49 @@ import * as React from 'react';
 import {View, Text} from 'react-native';
 
 import { createStackNavigator} from '@react-navigation/stack';
+import {TransitionSpecs} from '@react-navigation/stack';
 
 import Scanner from './pages/Scanner';
 import Home from './pages/Home';
 
 const Stack = createStackNavigator();
 
+const forFade = ({ current, closing }) => ({
+    cardStyle: {
+      opacity: current.progress,
+    },
+  });
+
 function Routes() {
     return(
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Scanner" component={Scanner} />
+        <Stack.Navigator 
+            initialRouteName="Main"
+            headerMode="screen"
+            
+            screenOptions={{
+            headerTintColor:'white',
+            headerStyle:{backgroundColor: 'blue',},
+            }}
+        >
+            <Stack.Screen name="Home" component={Home} 
+                options={
+                    {title:'Página Inicial',
+                    transitionSpec: {
+                        open: TransitionSpecs.TransitionIOSSpec,
+                        close: TransitionSpecs.TransitionIOSSpec, 
+                    },
+                    cardStyleInterpolator: forFade
+            }} 
+            />
+            <Stack.Screen name="Scanner" component={Scanner} 
+                options={{
+                    title:'Scanner',
+                    transitionSpec: {
+                        open: TransitionSpecs.TransitionIOSSpec,
+                        close: TransitionSpecs.TransitionIOSSpec, 
+                    },
+                    cardStyleInterpolator: forFade
+                }} />
 
         </Stack.Navigator>
     );
