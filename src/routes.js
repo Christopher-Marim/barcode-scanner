17575fb1a7021920694604ca4008b809d350f1e6
Christopher Marim <christopher.marim@etm.srv.br>
2020-03-07 15:05:46 -0300

    tab navigator problem

--- src/routes.js
+++ src/routes.js
@@ -3,62 +3,78 @@ import {View, Text} from 'react-native';
 
 import { createStackNavigator} from '@react-navigation/stack';
 import {TransitionSpecs} from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import Scanner from './pages/Scanner';
 import Home from './pages/Home';
 import Collect from './pages/Collect'
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const forFade = ({ current, closing }) => ({
     cardStyle: {
       opacity: current.progress,
     },
   });
+  
+  const HomeStack = createStackNavigator();
+  
+  function HomeStackScreen() {
+    return (
+      <HomeStack.Navigator>
+        <HomeStack.Screen name="Home" component={Home} 
+            options={
+                {title:'Página Inicial',
+                transitionSpec: {
+                    open: TransitionSpecs.TransitionIOSSpec,
+                    close: TransitionSpecs.TransitionIOSSpec, 
+                },
+                cardStyleInterpolator: forFade
+        }}/>
+        <HomeStack.Screen name="Scanner" component={Scanner} 
+            options={{
+                title:'Scanner',
+                transitionSpec: {
+                    open: TransitionSpecs.TransitionIOSSpec,
+                    close: TransitionSpecs.TransitionIOSSpec, 
+                },
+                cardStyleInterpolator: forFade
+        }} />
+        
+      </HomeStack.Navigator>
+    );
+  }
+  
+  const CollectStack = createStackNavigator();
+  
+  function CollectStackScreen() {
+    return (
+      <CollectStack.Navigator>
+        <Collect.Screen name="Collect" component={Collect} 
+            options={{
+                title:'Collect',
+                transitionSpec: {
+                    open: TransitionSpecs.TransitionIOSSpec,
+                    close: TransitionSpecs.TransitionIOSSpec, 
+                },
+                cardStyleInterpolator: forFade
+        }} />
+        
+      </CollectStack.Navigator>
+    );
+  }
+ 
+  
 
 function Routes() {
     return(
-        <Stack.Navigator 
-            initialRouteName="Home"
-            headerMode="screen"
-            
-            screenOptions={{
-            headerTintColor:'white',
-            headerStyle:{backgroundColor: 'blue',},
-            }}
-        >
-            <Stack.Screen name="Home" component={Home} 
-                options={
-                    {title:'Página Inicial',
-                    transitionSpec: {
-                        open: TransitionSpecs.TransitionIOSSpec,
-                        close: TransitionSpecs.TransitionIOSSpec, 
-                    },
-                    cardStyleInterpolator: forFade
-            }}/>
+        <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Collect" component={CollectStackScreen} />
+      </Tab.Navigator>
 
-            <Stack.Screen name="Collect" component={Collect} 
-                options={{
-                    title:'Collect',
-                    transitionSpec: {
-                        open: TransitionSpecs.TransitionIOSSpec,
-                        close: TransitionSpecs.TransitionIOSSpec, 
-                    },
-                    cardStyleInterpolator: forFade
-            }} />
-            
-            <Stack.Screen name="Scanner" component={Scanner} 
-                options={{
-                    title:'Scanner',
-                    transitionSpec: {
-                        open: TransitionSpecs.TransitionIOSSpec,
-                        close: TransitionSpecs.TransitionIOSSpec, 
-                    },
-                    cardStyleInterpolator: forFade
-            }} />
-
-            
-        </Stack.Navigator>
     );
 }
 
