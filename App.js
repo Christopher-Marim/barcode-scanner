e9cc0fded892c9d1bc041339bd2b1e3dcff20b96
Victor Hugo <pavictorhugo@gmail.com>
2020-03-07 00:49:05 -0300

    criada estrutura inicial do projeto

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button title="Go To Scan"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
